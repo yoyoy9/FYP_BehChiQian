f3eda71bacde596ad68a648dead69b368aefcadd
yoyoy9 <130053229+yoyoy9@users.noreply.github.com>
2024-09-05 23:03:28 +0800

    Update streamlit_forecast.py

--- streamlit_forecast.py
+++ streamlit_forecast.py
@@ -15,13 +15,14 @@ st.set_page_config(
     initial_sidebar_state="collapsed"
 )
 
-# Hide only the footer for a cleaner experience
-hide_footer_style = """
+# Hide Streamlit's default footer for a cleaner mobile experience
+hide_st_style = """
             <style>
+            #MainMenu {visibility: hidden;}
             footer {visibility: hidden;}
             </style>
             """
-st.markdown(hide_footer_style, unsafe_allow_html=True)
+st.markdown(hide_st_style, unsafe_allow_html=True)
 
 
 # Load the XGBoost Model
